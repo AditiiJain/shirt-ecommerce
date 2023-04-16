f0b7b29e7be1fdcd0a6671596e39b16d4210bbec
Aditi Jain <aditi.jain@hiration.com>
2023-04-16 15:59:30 +0530

    adding things to .env

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
+require("dotenv").config();
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  Navigate,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Nav, Error, SideSliderWrapper } from "./index";
-// import { AnimatePresence } from "framer-motion";
-
-// import Footer from "./components/Footer/Footer";
-
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { SET_PRODUCT_ITEMS } from "./redux/productItemsSlice";
-import {
-  FILTER_PRODUCTS,
-  SET_FILTER_ITEMS,
-  SORTING_PRODUCTS,
-} from "./redux/filterItemsSlice";
+import { SET_FILTER_ITEMS, SORTING_PRODUCTS } from "./redux/filterItemsSlice";
 import { getAllItems } from "./utils/firebaseFunctions";
 import { MainContainer, ProductDetails } from "./index";
 import { useShowSideSlider } from "./utils/useShowSideSlider";
-
-// const PrivateRoute = ({ children }) => {
-//   const user = useSelector((state) => state.user.user);
-//   return user !== null && user.email === "iaditi.jain08@gmail.com" ? (
-//     children
-//   ) : (
-//     <Navigate to="/" replace />
-//   );
-// };
 
 const App = () => {
   const dispatch = useDispatch();
@@ -39,7 +18,6 @@ const App = () => {
   const sorting_value = useSelector((state) => state.filterItems.sortingValue);
   const filterItems = useSelector((state) => state.filterItems.filterItems);
   const filters = useSelector((state) => state.filterItems.filters);
-  const allItems = useSelector((state) => state.filterItems.allItems);
   const productItems = useSelector((state) => state.productItems.productItems);
 
   const fetchItems = async () => {
@@ -72,17 +50,13 @@ const App = () => {
   }, [sorting_value, filters]);
 
   return (
-    // <AnimatePresence mode="wait">
     <div className="page-container">
       <Nav />
       <main className="main-container">
         <Outlet />
         {sideSliderShow && <SideSliderWrapper />}
       </main>
-
-      {/* <Footer /> */}
     </div>
-    // </AnimatePresence>
   );
 };
 
@@ -91,9 +65,7 @@ const appRouter = createBrowserRouter([
     path: "/",
     element: (
       <Provider store={store}>
-        {/* <FilterContextProvider> */}
         <App />
-        {/* </FilterContextProvider> */}
       </Provider>
     ),
     errorElement: <Error />,
@@ -106,14 +78,6 @@ const appRouter = createBrowserRouter([
         path: "/shirt/:shirtId",
         element: <ProductDetails />,
       },
-      // {
-      //   path: "/createItem",
-      //   element: (
-      //     <PrivateRoute>
-      //       <CreateItemContainer />
-      //     </PrivateRoute>
-      //   ),
-      // },
     ],
   },
 ]);
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,38 +1,24 @@
-require("dotenv").config();
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,"hi");
-
-// const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
-// const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-// const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
-// const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-// const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
-// const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-// const appId = process.env.REACT_APP_FIREBASE_APP_ID;
-
-// const firebaseConfig = {
-//   apiKey,
-//   authDomain,
-//   databaseURL,
-//   projectId,
-//   storageBucket,
-//   messagingSenderId,
-//   appId,
-// };
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_FIREBASE_APP_ID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtuO2BM9P-dDJVF2fj1CghalOsm4ES_z0",
-  authDomain: "shirt-ecommerce-3961d.firebaseapp.com",
-  databaseURL: "https://shirt-ecommerce-3961d-default-rtdb.firebaseio.com",
-  projectId: "shirt-ecommerce-3961d",
-  storageBucket: "shirt-ecommerce-3961d.appspot.com",
-  messagingSenderId: "619737108757",
-  appId: "1:619737108757:web:0302cd9e70aeb4ab41191c"
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
-
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
